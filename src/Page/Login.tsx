ed14ed2f2b0bcb12fa98c67b31865a1086cdeb0c
aria_bz <ariabahmanzangi@gmail.com>
2025-11-27 20:52:23 +0330

    v1

---
+++ src/Page/Login.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import logo from "../img/dark-logo.svg";
+
+const Login = () => {
+  return (
+    <div className="h-full bg-[#101214] text-white">
+      <div>
+        <img src={logo} alt="" />
+      </div>
+      <div>
+        <div>
+          <span>login ya register</span>
+          <p>hello,kjdkjjikhafjnlhudggfhgjjgj</p>
+          <div>
+            <span>+98|</span>
+            <input type="text" />
+          </div>
+        </div>
+        <div>
+          <button>tayeed va daryaft</button>
+          <button>vorod ba ramz vorod</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
