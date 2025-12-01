e359dd4ed4fd98cbad33df778b07edcac03b610d
aria_bz <ariabahmanzangi@gmail.com>
2025-12-01 13:48:41 +0330

    v2

--- src/Page/Login.tsx
+++ src/Page/Login.tsx
@@ -1,24 +1,73 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import logo from "../img/dark-logo.svg";
+import { useForm } from "react-hook-form";
+import Button2 from "../Components/Button2";
+import { useNavigate } from "react-router";
+import "../i18/index";
 
 const Login = () => {
+  const { t } = useTranslation();
+  const changePage = useNavigate();
+  const {
+    register,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      phoneNumber: "",
+      mane: "",
+    },
+  });
   return (
-    <div className="h-full bg-[#101214] text-white">
-      <div>
-        <img src={logo} alt="" />
-      </div>
-      <div>
-        <div>
-          <span>login ya register</span>
-          <p>hello,kjdkjjikhafjnlhudggfhgjjgj</p>
-          <div>
-            <span>+98|</span>
-            <input type="text" />
+    <div className="flex flex-col bg-[#0095da] w-full h-full pt-40 ">
+      <div className="flex flex-col  bg-white justify-between relative px-5 py-8 rounded-t-2xl h-full">
+        <div className="flex flex-col">
+          <img
+            className="w-[110px] h-[74px] absolute -top-2 left-50  -translate-1/2"
+            src={logo}
+            alt=""
+          />
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col justify-center gap-2">
+              <p>
+                {t("loginPage.register")}
+                <span className="text-gray-400"> | </span>{" "}
+                {t("loginPage.login")}
+              </p>
+              <p className="text-[#1f2937] text-[14px]">
+                {t("loginPage.hiPleaseEnterYourPhoneNumber")}
+              </p>
+            </div>
+            <div className="flex justify-center items-center py-3 pl-3 pr-8 rounded-md border border-gray-200">
+              <input
+                className="w-full  placeholder:text-gray-400 text-[15px] outline-none"
+                placeholder={t("loginPage.placeholder")}
+                {...register("phoneNumber", {
+                  minLength: 11,
+                  required: true,
+                })}
+                type="text"
+              />
+
+              <span className="flex justify-center text-gray-400">
+                <span>|</span> &#1784;<span></span>&#1785; <span>+</span>
+              </span>
+            </div>
           </div>
+          <p className="text-[14px] text-red-400 pt-4">
+            {errors.phoneNumber && <span>{t("loginPage.error")} </span>}
+          </p>
         </div>
-        <div>
-          <button>tayeed va daryaft</button>
-          <button>vorod ba ramz vorod</button>
+        <div className="flex flex-col gap-2">
+          <button
+            onClick={() => {
+              changePage("/Otp");
+            }}
+            className="bg-[#0095da] py-4 text-white rounded-md"
+          >
+            {t("loginPage.button1")}
+          </button>
+          <Button2 />
         </div>
       </div>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,21 @@ import "./index.css";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Login from "./Page/Login";
+import Otp from "./Page/otp";
+import MainPage from "./Page/MainPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Login />,
+  },
+  {
+    path: "/otp",
+    element: <Otp />,
+  },
+  {
+    path: "/MainPage",
+    element: <MainPage />,
   },
 ]);
 
